94282431b44c454b27ee7cd7fc3f5906b71a2b95
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2025-03-30 17:41:25 +0400

    update configs
    - update project name in configs
    - add wasm build script to support non-rust environments

---
+++ configs/scripts/build-wasm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if ! which wasm-pack; then
+  echo Init rustup
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -sSf | sh -s -- -y
+  sudo . $HOME/.cargo/env
+
+  echo Init wasm-pack
+  curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+fi
+
+rm -rf ./wasm-pkg
+echo Build wasm-pkg
+wasm-pack build \
+          --quiet \
+          --out-dir wasm-pkg \
+          --release
--- configs/scripts/build.sh
+++
@@ -1,8 +0,0 @@
-rm -rf ./wasm-pkg
-wasm-pack build \
-          --quiet \
-          --out-dir wasm-pkg \
-          --release
-
-yarn build:fe
-
