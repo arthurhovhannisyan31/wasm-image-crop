5361db3db08fff393a412b93dad9738e677475c8
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2025-04-20 00:43:56 +0400

    update configs
    - replace babel with ts-loader
    - add env variables
    - add incremental build for project and sw code
    - add sw package
    - extract common webpack config to base file
    - add assets-manifest generation to wp-prod config
    - add service worker wp config

--- configs/webpack/webpack.prod.js
+++ configs/webpack/webpack.prod.js
@@ -1,74 +1,44 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
+const { WebpackAssetsManifest } = require("webpack-assets-manifest");
+const { merge } = require("webpack-merge");
 
-const path = require("path");
+const baseConfig = require("./webpack.base");
 
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const excludedAssetsRegExp = new RegExp([
+  "index.html",
+  "manifest",
+  "service-worker",
+].join("|"), "gm");
+
+const prefetchedAssetsRegExp = /\.(mp4|webm)$/;
 
 require("dotenv").config();
 
-const isProd = process.env.__PROD__;
-const hash = isProd ? "contenthash:20" : "fullhash";
-
-module.exports = {
+/** @type { import('webpack').Configuration } */
+const prodConfig = {
   mode: "production",
-  target: ["web", "es2020"],
-  entry: [path.resolve("web", "index.tsx")],
-  optimization: {
-    minimize: true,
-  },
-  output: {
-    path: path.resolve("dist"),
-    filename: `[${hash}].[name].js`,
-    publicPath: "",
-    chunkFilename: `[${hash}].[name].chunk.js`,
-  },
-  experiments: {
-    topLevelAwait: true,
-    asyncWebAssembly: true,
-  },
-  module: {
-    rules: [
-      {
-        exclude: [/node_modules/],
-        test: /\.(ts|js)x?$/,
-        use: "babel-loader",
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: { outputPath: "public" },
-          },
-        ],
-      },
-    ],
-  },
   plugins: [
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: `[${hash}].[name].css`,
-    }),
-    new HtmlWebpackPlugin({
-      template: path.resolve("public", "index.html"),
-      cache: false,
-      favicon: path.resolve("public", "favicon.ico"),
+    new WebpackAssetsManifest({
+      enabled: true,
+      customize(entry, _, __, asset) {
+        if (entry.key.match(excludedAssetsRegExp)) {
+          return false;
+        }
+
+        return {
+          key: entry.key,
+          value: {
+            path: entry.value,
+            size: asset.source.size(),
+            prefetch: prefetchedAssetsRegExp.test(entry.key),
+          },
+        };
+      },
+      async done(manifest) {
+        await manifest.writeTo("public/assets-manifest.json");
+      },
     }),
   ],
-  resolve: {
-    plugins: [new TsconfigPathsPlugin()],
-    extensions: [".tsx", ".ts", ".js", ".jsx", ".mjs", ".json"],
-  },
-  stats: "minimal",
 };
+
+module.exports = merge(baseConfig, prodConfig);
