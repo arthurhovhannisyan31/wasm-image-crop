0e20c8358d257846dd0532d1d81b533324be9d0b
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2025-04-20 00:46:25 +0400

    add file server handler for service worker with proper headers

--- server.js
+++ server.js
@@ -10,9 +10,19 @@ const app = express();
 app.use(express.static(path.join(__dirname, "dist")));
 app.use("/", express.static(path.join(__dirname, "dist")));
 
+app.get("/service-worker.js", (_req, res) => {
+  res.set({
+    "Content-Type": "text/javascript",
+    "Cross-Origin-Opener-Policy": "same-origin",
+    "Cross-Origin-Embedder-Policy": "require-corp",
+  });
+  res.sendFile(path.join(__dirname, "public", "service-worker.js"));
+});
+
 app.get("/*", (_req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
+
 app.listen(PORT, () => {
   console.log(`The app is running on port http://localhost:${PORT}`);
 });
